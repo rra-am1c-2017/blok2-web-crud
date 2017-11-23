225dc110cd892894f001e85a02501fdca751bf9f
Arjan de Ruijter <rra.am1a.2017@gmail.com>
2017-11-23 12:14:47 +0100

    database backup

---
+++ database/crud.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Machine: 127.0.0.1
+-- Gegenereerd op: 23 nov 2017 om 12:12
+-- Serverversie: 5.6.17
+-- PHP-versie: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Databank: `am1c_2017_blok2_crud`
+--
+CREATE DATABASE IF NOT EXISTS `am1c_2017_blok2_crud` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `am1c_2017_blok2_crud`;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `voornaam` varchar(100) NOT NULL,
+  `tussenvoegsel` varchar(50) NOT NULL,
+  `achternaam` varchar(100) NOT NULL,
+  `leeftijd` tinyint(3) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `users`
+--
+
+INSERT INTO `users` (`id`, `voornaam`, `tussenvoegsel`, `achternaam`, `leeftijd`) VALUES
+(1, 'Arjan', 'de', 'Ruijter', 0),
+(3, 'Bas', 'de', 'Bakker', 0),
+(4, 'Teun', 'van', 'Vliet', 0),
+(5, 'Bert', 'de', 'Bakker', 0),
+(7, 'Hans', 'de', 'Beer', 0),
+(8, 'Hans', 'van', 'Manen', 0),
+(9, 'Test', 'de', 'Tester', 0),
+(10, 'Willem', 'van', 'Oranje', 0),
+(11, 'Donald', 'van der', 'Trump', 0),
+(12, 'Hansje', 'van den ', 'Bijstervelden', 0),
+(13, 'Hendrik', 'van den', 'Heuvel', 0),
+(14, 'Johan', '', 'Cruijff', 69),
+(15, 'Linda', 'de', 'Mol', 103),
+(16, 'Arjan', 'de', 'Ruijter', 56);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
